838dae9fcfa121c38bbd624d173e62c802c00aa6
Saif Abrar <saifabrar.webdev@gmail.com>
2023-06-09 00:11:22 +0600

    added remove user functionality

--- src/Pages/Dashboard/AllUsers.js
+++ src/Pages/Dashboard/AllUsers.js
@@ -1,47 +1,70 @@
 import React from 'react';
-import { useQuery } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import Loading from '../Shared/Loading/Loading';
 import UserTd from './UserTd';
+import { toast } from 'react-toastify';
 
 const AllUsers = () => {
-    const { data: users, isLoading, refetch } = useQuery('users', () => fetch('http://localhost:5000/user', {
+    const { data: users, isLoading, refetch } = useQuery('users', () =>
+      fetch('http://localhost:5000/user', {
         method: 'GET',
-        headers:{
-            authorization: `Bearer ${localStorage.getItem('accessToken')}`
-        }
-    }).then(res => res.json()));
-    if (isLoading) {
-        return <Loading></Loading>
-    }
-    
-    return (
-        <div>
-        <h2 className='text-xl font-bold text-primary py-2'>Total Users: {users.length}</h2>
-        <div className="overflow-x-auto">
-            <table className="table w-full">
-                <thead>
-                    <tr>
-                        <th></th>
-                        <th>Email</th>
-                        <th>Action</th>
-                        <th>Remove</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {
-                        users.map((user, index) => <UserTd
-                        key={user._id}
-                        user={user}
-                        index={index}
-                        refetch={refetch}
-                        ></UserTd>)
-                    }
-                </tbody>
-            </table>
-        </div>
-
-    </div>
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      }).then((res) => res.json())
     );
-};
-
-export default AllUsers;
+  
+    const deleteUser = useMutation((userId) =>
+      fetch(`http://localhost:5000/users/${userId}`, {
+        method: 'DELETE',
+        headers: {
+          authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        },
+      }).then((res) => res.json())
+    );
+  
+    const handleDeleteUser = async (userId) => {
+        try {
+          const response = await deleteUser.mutateAsync(userId);
+          toast.success('User successfully removed');
+          refetch();
+        } catch (error) {
+          toast.error('Error removing user');
+        }
+      };
+  
+    if (isLoading) {
+      return <Loading />;
+    }
+  
+    return (
+      <div>
+        <h2 className="text-xl font-bold text-primary py-2">Total Users: {users.length}</h2>
+        <div className="overflow-x-auto">
+          <table className="table w-full">
+            <thead>
+              <tr>
+                <th></th>
+                <th>Email</th>
+                <th>Action</th>
+                <th>Remove</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user, index) => (
+                <UserTd
+                  key={user._id}
+                  user={user}
+                  index={index}
+                  refetch={refetch}
+                  handleDeleteUser={handleDeleteUser}
+                />
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    );
+  };
+  
+  export default AllUsers;
--- src/Pages/Dashboard/UserTd.js
+++ src/Pages/Dashboard/UserTd.js
@@ -1,40 +1,51 @@
-import React from 'react';
-import { toast } from 'react-toastify';
+import React from "react";
+import { toast } from "react-toastify";
 
-const UserTd = ({ user, index, refetch }) => {
+const UserTd = ({ user, index, refetch, handleDeleteUser }) => {
+  const { email, role } = user;
+  const url = `http://localhost:5000/userx/admin/${email}`;
+  const makeAdmin = () => {
+    fetch(url, {
+      method: "PUT",
+      headers: {
+        authorization: `Bearer ${localStorage.getItem("accessToken")}`,
+      },
+    })
+      .then((res) => {
+        if (res.status === 403) {
+          toast.error("Failed to Make an admin");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        if (data.modifiedCount > 0) {
+          refetch();
+          toast.success(`Successfully made an admin`);
+        }
+      });
+  };
+  const handleRemoveUser = () => {
+    handleDeleteUser(user._id);
+  };
 
-    const { email, role } = user;
-    const url = `http://localhost:5000/userx/admin/${email}`;
-    const makeAdmin = () => {
-        fetch(url, { 
-            method: 'PUT',
-            headers: {
-                authorization: `Bearer ${localStorage.getItem('accessToken')}`
-            }
-        })
-        .then(res => {
-            if(res.status === 403){
-                toast.error('Failed to Make an admin');
-            }
-            return res.json()})
-        .then(data => {
-            if (data.modifiedCount > 0) {
-                refetch();
-                toast.success(`Successfully made an admin`);
-            }
-
-        })
-            
-    }
-    // 
-    return (
-        <tr>
-            <th>{index + 1}</th>
-            <td>{email}</td>
-            <td>{role !== 'admin' && <button onClick={makeAdmin} className="btn btn-xs">Make Admin</button>}</td>
-            <td><button className="btn btn-error btn-xs">Remove</button></td>
-        </tr>
-    );
+  return (
+    <tr>
+      <th>{index + 1}</th>
+      <td>{email}</td>
+      <td>
+        {role !== "admin" && (
+          <button onClick={makeAdmin} className="btn btn-xs">
+            Make Admin
+          </button>
+        )}
+      </td>
+      <td>
+        <button className="btn btn-error btn-xs" onClick={handleRemoveUser}>
+          Remove
+        </button>
+      </td>
+    </tr>
+  );
 };
 
 export default UserTd;
